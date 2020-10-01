3977687cfe0b84676088082b3e5a31179d77c821
abhishekdas <abhishekdas218@gmail.com>
2020-10-01 14:53:00 +0530

    Home Screen project structure is defined. (Updated)

--- app/src/main/java/com/idiot/smjewelscollector/MainActivity.java
+++ app/src/main/java/com/idiot/smjewelscollector/MainActivity.java
@@ -1,14 +1,49 @@
 package com.idiot.smjewelscollector;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.idiot.smjewelscollector.databinding.ActivityMainBinding;
+import com.idiot.smjewelscollector.mvp.ui.Dashboard.DashboardContract;
+import com.idiot.smjewelscollector.mvp.ui.Dashboard.DashboardPresenter;
+import com.idiot.smjewelscollector.mvp.utils.NavigationUtilMain;
+
+public class MainActivity extends AppCompatActivity implements DashboardContract.View {
+
+    DashboardContract.Presenter mPresenter;
+    ActivityMainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        mPresenter = new DashboardPresenter( this);
+        init();
+        mBinding.getRoot();
+    }
+
+    private void init() {
+        setupNavigator();
+        NavigationUtilMain.INSTANCE.setUpHomeScreen();
+    }
+
+    private void setupNavigator() {
+        NavigationUtilMain.INSTANCE.setupNavigator(this, getSupportActionBar(), getmBinding(), null);
+    }
+
+    public ActivityMainBinding getmBinding(){
+        return (ActivityMainBinding) mBinding;
+    }
+
+    @Override
+    public void onSuccess(String message) {
+
+    }
+
+    @Override
+    public void onFailure(String message) {
+
     }
 }
